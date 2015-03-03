3f649d2e1467441e617788d9ceee5c9b2c2e062d
DOTIN SCHOOL 3 <rsamira89@gmail.com>
2015-03-03 17:47:49 +0330

    session factory

---
+++ src/logic/SessionFactoryUtil.java
@@ -0,0 +1,53 @@
+package logic;
+
+/**
+ * Created by DOTIN SCHOOL 3 on 3/2/2015.
+ * @autor: Samira Rezaei
+ * because just we need one session factory , we used singleTone pattern...
+ */
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
+public class SessionFactoryUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null)
+            sessionFactory = buildSessionFactory();
+        return sessionFactory;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            System.out.println("Hibernate Configuration loaded");
+
+            //apply configuration property settings to StandardServiceRegistryBuilder
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate serviceRegistry created");
+
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            return sessionFactory;
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+
+}
